7657035de9548ff7cb1dd42c20160d91d4bb5a60
= <=>
2025-10-02 14:37:49 -0300

    commit inicial

---
+++ api-sistema-biblioteca/src/main/java/com/devteam/biblioteca/ApiSistemaBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.devteam.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaBibliotecaApplication.class, args);
+	}
+
+}
